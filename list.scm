2cc2814a0f07e5e64a086ed9445881761a3b9acd
JonCraig <jonathan.craig@aggiemail.usu.edu>
2021-09-30 16:11:16 -0600

    repl it is down, all code is currently untested

--- list.scm
+++ list.scm
@@ -10,26 +10,61 @@
         ((< n m) (gcd (- m n) n))
         (else m)))
 
-
+;; check appendages and verify base case
 (define (countingNumbers limit)
+  (cond (<= limit 1) (1)
+    (else (append (countingNumbers (- limit 1) limit)))
+  )
 )
 
-
+;; check that numbers are actually appending onto it
+;; note they are most likely not at the moment
 (define (evenNumbers limit)
+  (if (> limit 1) 
+    (cond (= (modulo limit 2) 0) 
+      (evenNumbers (- limit 1) limit)
+      (else (evenNumbers(- limit 1)))  
+    )
+  )
 )
 
-
+;; use a lambda function and recurse down to 0 appending on way back, or not
 (define (primeNumbers limit)
 )
 
+;; check sub list creation and usage
 (define (merge listOne listTwo)
+  (cond (>= (car listOne) (car listTwo)) 
+    append ((list (car listOne)) (merge (cdr listOne) listTwo))
+    (else (append(car listTwo) (merge listOne (cdr listTwo)))
+    )
+  )
 )
 
+
 (define (wrap numberToWrap aList)
+  (cond (>= numberToWrap 1) 
+    (wrap (- numberToWrap 1) (append (list (cdr aList)) (list (car aList))))
+    (else (aList))
+  )
 )
 
 (define (subLists aList)
+  (cond (null? ))
+
+  ;; list (count (cdr aList) aList)
 )
 
+;; this willl need a few things
+;; base case is to recurse down to initial value
+;; recurse may have to go down to 0, check my other functions allow
+;; function recurse ans current val
 (define (reduceLists func initialValue listOfLists)
+)
+
+(define (swap myList)
+   (if (>= (length myList) 2)
+      (append (list (cadr myList)) (list (car myList)) (cddr myList))
+      myList
+   )
 )
